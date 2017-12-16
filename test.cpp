898db7e9b48a944a1a238cf9c1389ac5eacb4f39
Unknown <ggarrison5@gmail.com>
2017-12-15 22:04:40 -0600

    test
    Testing improvements

--- test.cpp
+++ test.cpp
@@ -37,7 +37,34 @@ int main(){
         cout<<"File opening test failed!"<<endl;
         return -1;
     }
+    test.close();
     if(debug){
         cout<<"File opening test passed!"<<endl;
+    }
+    //add read debug
+    test.fileopen();
+    string testread;
+    getline(test.file,testread);
+    if (testread!="92spoons AI header"){
+        cout<<"File reading test failed!"<<endl;
+        return -1;
+    }
+    cout<<"File reading test passed!"<<endl;
+    srand(time(NULL));
+    string name = "test"+to_string(rand());
+    test.add((name+":test").c_str());
+    test.close();
+    test.read();
+    vector<string>::iterator it;
+    it = find (test.first.begin(), test.first.end(), name);
+    if (it == test.first.end()){
+        cout<<"File writing test failed!"<<endl;
+        return -1;
+    }else{
+        //TODO: Fix this conversion error
+        long nPosition = distance (test.first.begin (), it);
+        if (debug){
+        cout << "File writing test passed!"<<endl;
+        }
     }
 }
